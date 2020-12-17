6c150c5c07da7ac61b59f0f3d2758fea60a99cd6
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-17 10:31:04 -0500

    idkWhyItsNotWorking

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import { Component } from 'react'
 
-const url  = 'http://www.omdbapi.com/?i=tt3896198&apikey=429f9e0f'
+const url  = 'http://www.omdbapi.com/?apikey=[429f9e0f]&'
+const postUrl = 'http://img.omdbapi.com/?apikey=429f9e0f&'
 
 export class App extends Component{
 
   state = {
-    movie: ''
+    movies: ''
   }
 
-  getMovies = data => fetch(data).then(res => res.json()).then(movie => this.setState({ movie }))
+  getMovies = data => fetch(data).then(res => res.json()).then(movies => this.setState({ movies }))
 
   componentDidMount(){
     this.getMovies(url)
   }
 
   render(){
-    console.log(this.state.movie)
+    console.log(this.state.movies)
     return (
       <div className="App">
         <h2>Hi Greg</h2>
