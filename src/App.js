b73584731294fbd9d61752f37c2138dd48837170
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-17 11:02:32 -0500

    stillworking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import './App.css';
 import { Component } from 'react'
 
-const url  = 'http://www.omdbapi.com/?apikey=[429f9e0f]&'
-const postUrl = 'http://img.omdbapi.com/?apikey=429f9e0f&'
-
 export class App extends Component{
 
   state = {
-    movies: ''
+    move: '',
+    input: ''
   }
 
-  getMovies = data => fetch(data).then(res => res.json()).then(movies => this.setState({ movies }))
-
-  componentDidMount(){
-    this.getMovies(url)
+  getMovies = (key, movie) => {
+    fetch(`http://www.omdbapi.com/?apikey=${key}&s=${movie}`).then(res => res.json()).then(movies => this.setState({ movies }))
   }
+
+  handleOnchage = e => this.setState({ [e.taget.name]: e.target.value })
+
+
+  handleSearch = e => console.log(this.state.input)
 
   render(){
     console.log(this.state.movies)
     return (
       <div className="App">
-        <h2>Hi Greg</h2>
+        <h2>Top Five Movies</h2>
+        <form>
+          <input onChange={this.handleOnchage} name='input' placeholder='Search Movie...'>{this.state.input}</input>
+        </form>
+        <button onClick={this.handleSearch}>Search For Movie</button>
       </div>
     )
   }
