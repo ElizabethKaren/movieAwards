36f306d11cbabe56b7df106c29056768b4362a68
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-01-13 15:06:44 -0500

    functionalityWorking

--- src/Componants/TitleOfMovie.js
+++ src/Componants/TitleOfMovie.js
@@ -6,7 +6,7 @@ const TitleOfMovie = props => {
     return (
         <div className='eachMovie'>
             <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><p>{props.Title} - {year}</p></Link>
-            { props.Poster === 'N/A' ? null : <Link to={`/movie/{${props.Title}`}><img src={props.Poster} alt={props.Title} /></Link> }
+            { props.Poster === 'N/A' ? null : <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><img src={props.Poster} alt={props.Title} /></Link> }
         </div>
     )
 }
