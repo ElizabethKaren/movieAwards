185dfa83728b0d5d40edca8cd3f8eea57d8f5fa5
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-01-13 14:48:37 -0500

    posterAdded

--- src/Componants/TitleOfMovie.js
+++ src/Componants/TitleOfMovie.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const TitleOfMovie = props => {
+    console.log(props)
     return (
         <div>
-            <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><h3>{props.Title}</h3></Link>
+            <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><p>{props.Title} - {props.Year}</p></Link>
+            { props.Poster === 'N/A' ? null : <Link to={`/movie/{${props.Title}`}><img src={props.Poster} alt={props.Title} /></Link> }
         </div>
     )
 }
