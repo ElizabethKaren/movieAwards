7c956bd56075866a8f58749b4caf92fa196b77b7
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-01-13 14:58:55 -0500

    yearFixed

--- src/Componants/TitleOfMovie.js
+++ src/Componants/TitleOfMovie.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const TitleOfMovie = props => {
-    console.log(props)
+    const year = parseInt(props.Year )
     return (
         <div className='eachMovie'>
-            <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><p>{props.Title} - {props.Year}</p></Link>
+            <Link to={`/movie/{${props.Title}`} onClick={() => props.movieClicked(props)}><p>{props.Title} - {year}</p></Link>
             { props.Poster === 'N/A' ? null : <Link to={`/movie/{${props.Title}`}><img src={props.Poster} alt={props.Title} /></Link> }
         </div>
     )
