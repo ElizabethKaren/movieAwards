1e9e461f56a240e0a13f33cf2f68515d31a0ddfd
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-01-17 19:05:59 -0500

    whyISbreakNotWorking

--- src/Componants/MovieList.js
+++ src/Componants/MovieList.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import TitleOfMovie from './TitleOfMovie'
+import { Link } from 'react-router-dom'
 
 const MovieList = props => {
     if (props.myTopFive === null && props.list === null) return <div></div>
-    if (props.myTopFive) return <div className='movieList'><br/><br/>{props.myTopFive.map(movie => <TitleOfMovie movieClicked={props.movieClicked} {...movie} key={movie.imdbID}/> )}</div>
+    if (props.myTopFive) return (
+        <div className='movieList'>
+            <Link to='/'><p>X</p></Link>
+            <br></br>
+            <br></br>
+            {props.myTopFive.map(movie => <TitleOfMovie movieClicked={props.movieClicked} {...movie} key={movie.imdbID}/> )}
+        </div>
+    )
+    if (!props.list) return <div></div>
     const list = props.list.Search
     if (!list) return <div></div>
     if (props.list === '' || props.list === null) return <div></div>
